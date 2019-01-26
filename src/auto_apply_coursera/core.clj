6236b88a6ef3d39ef3c8710159915bdf7036b631
Abhinav Sharma <abhi18av@gmail.com>
2019-01-26 11:08:31 +0530

    working directly on the core  file now

--- src/auto_apply_coursera/core.clj
+++ src/auto_apply_coursera/core.clj
@@ -1,6 +1,14 @@
 (ns auto-apply-coursera.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(ns auto-apply-coursera.core
+  (:use etaoin.api)
+  (:require [etaoin.keys :as k]))
+
+
+;; NOTE: https://github.com/igrishaev/etaoin
+
+
+;; DONE Find out how the custom locations work
+(def driver (firefox {:path-browser "/Applications/Firefox Developer Edition.app/Contents/MacOS/firefox"}))
+
+
